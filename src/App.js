ce63c10e93d7515edc9d633badbf1fdaa6c5d37f
Hasan Oztunc <hasanhuseyin9e@gmail.com>
2021-10-20 22:54:57 +0300

    title was changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hasan Oztuncs Blog with React and Github Pages
         </p>
         <a
           className="App-link"
